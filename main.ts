54990a327e7c3f4f21b4e8c8147f671a92e483db
Hendrick Menacho <hmenacho97@gmail.com>
2023-10-16 14:48:14 -0300

    primeiro commit

---
+++ main.ts
@@ -0,0 +1,10 @@
+function calculoDaArea(base: number, altura: number): number {
+  return base * altura;
+}
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(calculoDaArea(15, 20));
+console.log(saudacao("João"));
