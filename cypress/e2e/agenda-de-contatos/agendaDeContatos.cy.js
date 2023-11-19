965024f37500bb0cc3a40ea36f8d10c030ac2685
Hendrick Menacho <hmenacho97@gmail.com>
2023-11-19 15:24:06 -0300

    colocando todos os testes em um arquivo

--- cypress/e2e/agenda-de-contatos/agendaDeContatos.cy.js
+++ cypress/e2e/agenda-de-contatos/agendaDeContatos.cy.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
-describe("testando a alteração de um contato", () => {
+describe("testando a inclusão de um contato", () => {
   beforeEach(() => {
     cy.visit("https://agenda-contatos-react.vercel.app");
+  });
+  it("deve adicionar um contato na agenda", () => {
+    cy.get('input[placeholder="Nome"]').type("Hendrick");
+    cy.get('input[placeholder="E-mail"]').type("hendrick@gmail.com");
+    cy.get('input[placeholder="Telefone"]').type("71988557733");
+    cy.get(".adicionar").click();
   });
   it("deve alterar e salvar um contato existente(Bruna Costa)", () => {
     cy.get(":nth-child(3) > .sc-gueYoa > .edit").click();
@@ -15,5 +21,10 @@ describe("testando a alteração de um contato", () => {
     cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
     cy.get('input[placeholder="Telefone"]').clear().type("15932635189");
     cy.get(".cancelar").click();
+  });
+  it("deve remover os contatos", () => {
+    cy.get(":nth-child(2) > .sc-gueYoa > .delete").click();
+    cy.get(":nth-child(3) > .sc-gueYoa > .delete").click();
+    cy.get(":nth-child(4) > .sc-gueYoa > .delete").click();
   });
 });
